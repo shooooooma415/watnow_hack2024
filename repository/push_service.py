4a4059cbb42b96659b28351286bb54b03bc96e56
shooooooma415 <shoma20040415@icloud.com>
2024-09-20 11:24:06 +0900

    fix: fix get_token

--- repository/push_service.py
+++ repository/push_service.py
@@ -47,29 +47,31 @@ class PushService():
                 option_id_list.append(row['id'])
         return option_id_list
     
-    def get_user_id(self, option_id:str)->List[str]:
+    def get_token(self, option_id_list:List[str])->List[str]:
         token_list = list()
-        with self.engine.connect() as conn:
-            query = conn.execute(
-                text("""SELECT 
-                        u.token
-                        FROM votes v
-                        JOIN users u
-                        ON v.user_id = u.id
-                        WHERE v.option_id = :id
-                        """)),{
-                            "id":option_id
-                        }
-            for row in query:
-                token_list.append(row['id'])
+        for option_id in option_id_list:
+            with self.engine.connect() as conn:
+                query = conn.execute(
+                    text("""SELECT 
+                            u.token
+                            FROM votes v
+                            JOIN users u
+                            ON v.user_id = u.id
+                            WHERE v.option_id = :id
+                            """)),{
+                                "id":option_id
+                            }
+                for row in query:
+                    token_list.append(row['id'])
         return token_list
     
     def send_notofication(self,event_id:str):
+        
         with self.engine.connect() as conn:
             query = conn.execute(text("SELECT title FROM events WHERE id = :id")),{"id": event_id}
         result = query.fetchone()
         event_title = result['title']
-        token_list = self.get_user_id(self.get_option_id(event_id))
+        token_list = self.get_token(self.get_option_id(event_id))
         message = messaging.Message(
             notification=messaging.Notification(
                 title=event_title,

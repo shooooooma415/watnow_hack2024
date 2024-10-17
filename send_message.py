31e5ede139bd12d003ad0f976271dd3c7de758f2
shooooooma415 <shoma20040415@icloud.com>
2024-10-18 03:37:47 +0900

    add: グローバル変数の実装

--- send_message.py
+++ send_message.py
@@ -1,7 +1,6 @@
 from service.notification import Notification
 import os
 import requests
-from main import today_event_id_list
 
 SERVER_URL = "http://127.0.0.1:8000"
 
@@ -9,13 +8,11 @@ SERVER_URL = "http://127.0.0.1:8000"
 supabase_url = os.getenv('SUPABASE_URL')
 notification=Notification(supabase_url)
 
-# def send_event_id(event_id: int):
-#     response = requests.post(f"{SERVER_URL}/events/id", json={"event_id": event_id})
-#     return response
+def send_event_id(event_id: int):
+    response = requests.post(f"{SERVER_URL}/events/id", json={"event_id": event_id})
+    return response
 
-t = notification.send_messages()
-print(t)
-# for id in event_id_list:
-#     send_event_id(id)
-    
-# print(today_event_id_list)
+event_id_list = notification.send_messages()
+
+for id in event_id_list:
+    send_event_id(id)

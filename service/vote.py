b0ae3900031205bb84423c0a44d6a2017b565772
shooooooma415 <shoma20040415@icloud.com>
2024-10-24 18:10:25 +0900

    fix: 途中参加の投票機能を修正

--- service/vote.py
+++ service/vote.py
@@ -9,7 +9,7 @@ class Vote():
     def delete_vote(self, event_id:int, user_id:int):
         sanka_id = self.get_attendance.get_option_id(event_id, "参加")
         husanka_id = self.get_attendance.get_option_id(event_id, "不参加")
-        totyuukara_id = self.get_attendance.get_option_id(event_id, "参加")
+        totyuukara_id = self.get_attendance.get_option_id(event_id, "途中から参加")
         
         if self.get_attendance.is_option(user_id, sanka_id):
             self.add_votes.delete_vote(sanka_id,user_id)
